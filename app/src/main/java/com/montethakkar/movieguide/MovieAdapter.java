5010eaddeee12c0f2e9ae421d88519ccff7aaa16
Monte9 <Monte9@users.noreply.github.com>
2016-10-25 21:48:59 -0700

    added layouts; loads images; desired outcome but looks shitty

--- app/src/main/java/com/montethakkar/movieguide/MovieAdapter.java
+++ app/src/main/java/com/montethakkar/movieguide/MovieAdapter.java
@@ -5,7 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -22,16 +25,26 @@ public class MovieAdapter extends ArrayAdapter<Movie> {
     public View getView(int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
         Movie movie = getItem(position);
+
+        String baseImageURL = "http://image.tmdb.org/t/p/w500";
+
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.movie_row, parent, false);
         }
+
         // Lookup view for data population
         TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
         TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+        ImageView imagePoster = (ImageView) convertView.findViewById(R.id.posterImage);
+        ImageView imageBackdrop = (ImageView) convertView.findViewById(R.id.backdropImage);
+
         // Populate the data into the template view using the data object
         tvName.setText(movie.title);
         tvOverview.setText(movie.overview);
+        Picasso.with(this.getContext()).load(baseImageURL + movie.backdrop_path).into(imageBackdrop);
+        Picasso.with(this.getContext()).load(baseImageURL + movie.poster_path).into(imagePoster);
+
         // Return the completed view to render on screen
         return convertView;
     }
